2a730294e499ad2cce2b552d32c7650cf3ed1df4
ZBrinz <ZachBrinsley@gmail.com>
2021-09-01 17:38:23 -0700

    updated requirements for user and inventory and added sync at bottom of app.js

--- models/inventory.js
+++ models/inventory.js
@@ -3,7 +3,7 @@ const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Inventory extends Model {
     static associate(models) {
-      // define association here
+      this.belongsTo(models.User);
     }
   }
   Inventory.init(
