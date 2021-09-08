71677909e8818746ca208d0c359dcc3d31c4e3bf
Steven <sgalland@mail.com>
2021-09-07 18:20:30 -0700

    added cors and files to help cors run

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var models = require("./models");
+var cors = require("cors");
 
 var auth = require("./services/auth");
 
@@ -16,11 +17,20 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(cors());
 
 // error handler
 app.use(function (err, req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -5,9 +5,9 @@ var bcrypt = require("bcrypt");
 var auth = require("../services/auth");
 
 // /* GET users listing. */
-// router.get("/", function (req, res, next) {
-//   res.send("respond with a resource");
-// });
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
 
 // Post, Register User
 router.post("/", async (req, res, next) => {
